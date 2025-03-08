7ed3bd90d132bb4b8b4130fd7705946156cc8b05
watorutart <watoru374215@gmail.com>
2025-03-08 23:39:37 +0900

    refactor: :recycle: OASを作成中

--- app/api/[...route]/route.ts
+++ app/api/[...route]/route.ts
@@ -1,24 +1,66 @@
-import { Hono } from 'hono'
-import { handle } from 'hono/vercel'
-import testFormRouter from './routes/test-form'
-import {zValidator} from '@hono/zod-validator'
-import {z} from 'zod';
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
+import testFormRouter from "./routes/test-form";
+import { zValidator } from "@hono/zod-validator";
+import { z } from "zod";
+import { describeRoute, openAPISpecs } from "hono-openapi";
 
-export const runtime = 'edge'
+export const runtime = "edge";
 
 // appをエクスポートして、テストでアクセスできるようにする
-export const app = new Hono().basePath('/api')
+export const app = new Hono().basePath("/api");
 
-app.get('/hello', zValidator('query', z.object({ name: z.string() })), (c) => {
-  const { name } = c.req.valid('query');
+// TODO: OASを生成したいがまだ途中
+// const querySchema = z.object({
+//   name: z.string(),
+// });
 
-  return c.json({
-    message: `Hello! ${name}`
-  })
-})
+// app.get(
+//   "/openapi",
+//   openAPISpecs(app, {
+//     documentation: {
+//       title: "My API",
+//       version: "1.0.0",
+//       description: "My API description",
+//     },
+//     servers: [
+//       {
+//         url: "http://localhost:3003",
+//         description: "Local server",
+//       },
+//     ],
+//   })
+// );
+
+app.get(
+  "/hello",
+  describeRoute({
+    description: "Helloエンドポイント",
+    responses: {
+      200: {
+        description: "成功",
+        content: {
+          "application/json": {
+            schema: z.object({
+              message: z.string(),
+            }),
+          },
+        },
+      },
+    },
+  }),
+  zValidator("query", querySchema),
+  (c) => {
+    const { name } = c.req.valid("query");
+
+    return c.json({
+      message: `Hello! ${name}`,
+    });
+  }
+);
 
 // /test/form エンドポイントをマウント
-app.route('/test/form', testFormRouter)
+app.route("/test/form", testFormRouter);
 
-export const GET = handle(app)
-export const POST = handle(app)
+export const GET = handle(app);
+export const POST = handle(app);
